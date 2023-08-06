1814a52a68673579859bb255c28bb294bad36582
SupraConstruct <83255856+beefrolls@users.noreply.github.com>
2023-08-07 00:01:21 +0800

    Added read status button & delete button functionalities, removed old delete button on the upper right
    Created separate div class ('options' div) for read status and delete. Added appropriate code and styles.
    
    Removed delete button belonging to 'right' div as visible on the upper right section of the window

--- code.js
+++ code.js
@@ -50,6 +50,7 @@ const bookAuthor = document.querySelector('#author');
 const bookPageCnt = document.querySelector('#page-count');
 const bookReadStat = document.querySelector('#read-status');
 const libShelf = document.querySelector('.lib-main');
+const readStatBtn = document.querySelector('.read-status');
 
 //This code opens and closes the add book menu
 
@@ -86,15 +87,18 @@ confirmBookBtn.addEventListener('click', function() {
         let newBookTitle = document.createElement('h3');
         let newBookAuthor = document.createElement('p');
         let newBookPgCnt = document.createElement('p');
+        let newBookOpt = document.createElement('div');
         let newBookRdStat = document.createElement('button');
+        let newBookDel = document.createElement('button');
 
 
         newBook.classList.add('book');
         newBookTitle.classList.add('title');
         newBookAuthor.classList.add('author');
         newBookPgCnt.classList.add('page-count');
+        newBookOpt.classList.add('options');
         newBookRdStat.classList.add('read-status');
-        
+        newBookDel.classList.add('delete');
 
         newBookTitle.textContent = `${book['title']}`; 
         newBookAuthor.textContent = `${book['author']}`;
@@ -106,13 +110,36 @@ confirmBookBtn.addEventListener('click', function() {
                 } else {
                     newBookRdStat.setAttribute('id', 'not-read');
                 }
+        newBookDel.textContent = 'Del';
 
-        newBook.appendChild(newBookRdStat); 
+        //Adds event listeners to elements: Read Status & Delete
+        //Changes the read status on click (read -> not read, vise versa)
+        newBookRdStat.addEventListener('click', function() {
+            if (newBookRdStat.textContent == 'Read') {
+                newBookRdStat.setAttribute('id', 'not-read');
+                newBookRdStat.textContent = 'Not Read';
+            } else {
+                newBookRdStat.setAttribute('id', 'read');
+                newBookRdStat.textContent = 'Read';
+            }
+        })
+
+        //Deletes the book on click
+        newBookDel.addEventListener('click', function(e) {
+            (newBookDel.parentNode).parentNode.remove()
+        })
+
+        newBookOpt.appendChild(newBookRdStat);
+        newBookOpt.appendChild(newBookDel);
+
+        newBook.appendChild(newBookOpt);
         newBook.appendChild(newBookTitle);
         newBook.appendChild(newBookAuthor);
-        newBook.appendChild(newBookPgCnt);
+        newBook.appendChild(newBookPgCnt)
+        
         
         libShelf.appendChild(newBook);
+        
         
     } else {
         console.log('Please fill up the required fields')
